8243007fc88d861f4696dcb683e989a7deeae4f5
lmotp <lmotp@daum.net>
2022-03-11 23:04:52 +0900

    캐러셀,프로덕트내용 추가

---
+++ js/productCarousel.js
@@ -0,0 +1,12 @@
+const slider = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  nav: true,
+  navContainer: '.product-carousel .thumbnail-list',
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
