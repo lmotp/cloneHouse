75bb4817f0a4905def6bad88a28d44f84dddca7d
lmotp <lmotp@daum.net>
2022-03-16 23:32:29 +0900

    자스 전까지 레이아웃 완성

---
+++ js/carousels.js
@@ -0,0 +1,40 @@
+const slider = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  nav: true,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
+
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
+  controls: false,
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
+  navAsThumbnails: true,
+  gutter: 4,
+  edgePadding: 16,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
+
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  navAsThumbnails: true,
+  gutter: 6,
+  edgePadding: 75,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
